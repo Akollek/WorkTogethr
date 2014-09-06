ec202723fb3b3a301f53e23e8b7fe08786604544
Shubhagata Sengupta <shubsengupta@gmail.com>
2014-09-06 01:58:14 -0400

    init project

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/worktogethr-dev'
+  },
+
+  seedDB: true
+};
