917ee8f36dd7bf7d73d01f65f493af8219ce79e7
Daniel Que <dmdque@gmail.com>
2014-09-06 13:10:19 -0400

    added course stuff

---
+++ server/api/course/course.model.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CourseSchema = new Schema({
+  name: String
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
